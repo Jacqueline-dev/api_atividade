634b9c9a18b6d2564dab5da4ab8493e2bb95fe02
Jacqueline-dev <jacque.ferreira@outlook.com.br>
2023-05-15 16:00:35 -0300

    criação banco de dados

---
+++ models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///atividade.bb', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocmmit=False,
+                                         bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
