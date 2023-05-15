eaa5a76e08bcc31901b1721312a1d9916fd74375
Jacqueline-dev <jacque.ferreira@outlook.com.br>
2023-05-15 16:24:45 -0300

    criação das classes Pessoas e Atividades

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import (create_engine, Column, Integer, String, ForeignKey)
+from sqlalchemy.orm import (scoped_session, sessionmaker, relationship)
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///atividade.bb', convert_unicode=True)
@@ -8,4 +8,22 @@ db_session = scoped_session(sessionmaker(autocmmit=False,
 
 Base = declarative_base()
 Base.query = db_session.query_property()
+
+
+class Pessoas(Base):
+    __tablename__ = 'pessoas'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(40), index=True)
+    idade = Column(Integer)
+
+    def __repr__(self):
+        return '<Pessoa {}>'.format((self.nome))
+
+
+class Atividades(Base):
+    __tablename__ = 'atividades'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(80))
+    pessoa_id = Column(Integer, ForeignKey('pessoa.id'))
+    pessoa = relationship(Pessoas)
 
