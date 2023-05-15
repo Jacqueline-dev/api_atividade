a17fb714a0fc0f4d6061294936cc1b5f16c45b00
Jacqueline-dev <jacque.ferreira@outlook.com.br>
2023-05-15 20:26:56 -0300

    adicionado models com tabelas e criado um utils para manipular dados no banco de dados

---
+++ utils.py
@@ -0,0 +1,33 @@
+from models import Pessoas
+
+
+# Insere dados na tabela pessoa
+def insere_pessoas():
+    pessoa = Pessoas(nome='Bruno', idade=29)
+    print(pessoa)
+    pessoa.save()
+
+# Realiza consulta na tbela pessoa
+def consulta_pessoas():
+    pessoas = Pessoas.query.all()
+    # pessoa = Pessoas.query.filter_by(nome='Bruno').first()
+    # print(pessoa.idade)
+    print(pessoas)
+
+# Altera dados na tabela pessoa
+def altera_pessoa():
+    pessoa = Pessoas.query.filter_by(nome='Bruno').first()
+    pessoa.idade = 21
+    pessoa.save()
+
+# Exclui dados na tabela pessoa
+def excluir_pessoa():
+    pessoa = Pessoas.query.filter_by(nome='Jacque').firts()
+    pessoa.delete()
+
+
+if __name__ == '__main__':
+    # insere_pessoas()
+    # consulta_pessoas()
+    # altera_pessoa()
+    excluir_pessoa()
